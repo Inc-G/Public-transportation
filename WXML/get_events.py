faa94539a04e8ce057f50d03e4a8d376cd3509fe
nicolastruong99 <nicolastr99@outlook.com>
2025-05-22 18:02:12 -0700

    Update get_events.py

--- WXML/get_events.py
+++ WXML/get_events.py
@@ -1,16 +1,21 @@
-def get_events(nodes):
+def get_events(columns, rows):
     """
     nodes: Vector of (Vehicle, Station) pairs
 
     return: Set of all events from corresponding nodes
     """
 
-    events = set()
+    column_events = set()
+    row_events = set()
 
-    for node in nodes:
-        events.add(node + (-1,))
-        events.add(node + (1,))
+    for columns in columns:
+        column_events.add(column + (-1,))
+
+    for row in rows:
+        row_evenvts.add(row + (1,))
+
+    events = column_events.union(row_events)
     
-    return events
+    return column_events, row_events, events
 
 print(get_events([(0, 0), (0, 1), (1, 3), (1, 1)]))
